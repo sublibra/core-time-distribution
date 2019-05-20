065fc0a81f1e8414f9cbdca1fac618ac592c576b
Jonas Hörström <jonas.horstrom@gmail.com>
2019-05-20 11:31:21 +0200

    Fix code duplication in kpi

--- src/kpi.jsx
+++ src/kpi.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './kpi.scss';
+import { createReadStream } from 'fs';
 
 export default function useKpi(layout, app) {
  
@@ -19,28 +20,22 @@ export default function useKpi(layout, app) {
     setSelected(!selected);
   };    
 
+  const cards = () => {
+   let res = [];
+    for (let i=0; i<layout.qHyperCube.qMeasureInfo.length; i++){
+      let className = "card card-" + (i+1);
+      res.push(
+        <div className={className} onClick={() => selectType(layout.qHyperCube.qMeasureInfo[i].qFallbackTitle)}>
+        <p className="name">Average <br /><span>{layout.qHyperCube.qMeasureInfo[i].qFallbackTitle}</span></p>
+        <p className="num">{Math.trunc(layout.qHyperCube.qGrandTotalRow[i].qText * 100)}%</p>
+      </div>)
+    }
+    return res;
+  }
+
   return (
     <div className="list">
-
-      <div className="card card-1" onClick={() => selectType('feature')}>
-        <p className="name">Average <br /><span>{layout.qHyperCube.qMeasureInfo[0].qFallbackTitle}</span></p>
-        <p className="num">{Math.trunc(layout.qHyperCube.qGrandTotalRow[0].qText * 100)}%</p>
-      </div>
-
-      <div className="card card-2" onClick={() => selectType('enhancement')}>
-        <p className="name">Average <br /><span>{layout.qHyperCube.qMeasureInfo[1].qFallbackTitle}</span></p>
-        <p className="num">{Math.trunc(layout.qHyperCube.qGrandTotalRow[1].qText * 100)}%</p>
-      </div>
-
-      <div className="card card-3" onClick={() => selectType('maintenance')}>
-        <p className="name">Average <br /><span>{layout.qHyperCube.qMeasureInfo[2].qFallbackTitle}</span></p>
-        <p className="num">{Math.trunc(layout.qHyperCube.qGrandTotalRow[2].qText * 100)}%</p>
-      </div>
-
-      <div className="card card-4" onClick={() => selectType('admin')}>
-        <p className="name">Average <br /><span>{layout.qHyperCube.qMeasureInfo[3].qFallbackTitle}</span></p>
-        <p className="num">{Math.trunc(layout.qHyperCube.qGrandTotalRow[3].qText * 100)}%</p>
-      </div>
+      {cards()}
     </div>
   );
 }
